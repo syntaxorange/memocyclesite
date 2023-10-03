1648e3bf9c45bfdf43994b1a9e126a5d02de6da6
syntaxorange <syntaxorange@gmail.com>
2023-10-03 17:16:04 +0300

    add desktop markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,90 @@
 import Image from 'next/image'
+import { BellIcon, ClockIcon, LightBulbIcon, GlobeAltIcon, BoltIcon } from '@heroicons/react/24/solid'
 
 export default function Home() {
+  const year = (new Date()).getFullYear();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="flex flex-col items-center justify-between">
+      <div className="header max-w-screen-xl w-full items-center justify-between lg:flex">
+        <a href="/">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            className="dark:invert"
+            width={163}
+            height={30}
+            priority
+          />
+        </a>
+        <ul className="flex text-white menu font-light">
+          <li className="mr-7">
+            <a className="hover:underline underline-offset-4 decoration-1" href="#about">О расширении</a>
+          </li>
+          <li className="mr-7">
+            <a className="hover:underline underline-offset-4 decoration-1" href="#benefits">Преимущества</a>
+          </li>
+          <li>
+            <a href="#" className="text-lg font-bold py-3 px-7 border border-white rounded-xl hover:bg-slate-50/5 active:bg-slate-50/10">Установить</a>
+          </li>
+        </ul>
+      </div>
+      <div className="max-w-screen-xl mb-20">
+        <div className="main-screen flex">
+          <div className="left">
+            <div className="main-text text-white relative mb-8">Topic Meister - ваш ключ к эффективному обучению!</div>
+            <div className="main-text-bottom text-white text-2xl">Расширение доступно для браузеров Chrome и Edge.</div>
+            <a href="#" className="install-btn text-lg font-bold py-4 px-9 inline-block bg-white rounded-xl hover:bg-slate-50 active:bg-slate-100">Установить</a>
+          </div>
+          <div>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/owl.png"
+              className="main-img"
+              alt="owl"
+              width="474"
+              height="702"
             />
-          </a>
+          </div>
         </div>
+        <article>
+          <div className="title relative text-center font-bold mb-4" id="about">О расширении</div>
+          <p>Вы устали от множества открытых вкладок, бесконечного скроллинга и потери концентрации во время изучения новых тем? "Topic Meister" - это инновационное расширение для вашего браузера, которое сделает процесс обучения более увлекательным, упорядоченным и продуктивным!</p>
+          <p>Часто мы заполняем нашу память информацией, которая забывается в течение короткого времени. <span className="underline underline-offset-4">Но с "Topic Meister" каждая новая тема, которую вы изучаете, станет по-настоящему запоминающейся!</span> Создавайте собственные топики и заполняйте их концепциями, которые вы хотите усвоить - будь то ключевые понятия из учебника, новые термины или факты.</p>
+          <p>Функциональность "Topic Meister" включает грамотное интегрирование концепций в ваш браузерный опыт. Настроив таймер, вы можете установить интервалы появления попап-окон с концепциями непосредственно на страницах, которые вы просматриваете. Вместо того, чтобы отвлекаться на переключение между вкладками или поиском дополнительной информации, "Topic Meister" привлечет ваше внимание к существенным понятиям, прямо там, где вы уже находитесь!</p>
+          <p>Используйте "Topic Meister" для любого предмета или области знаний. Будь то учебные материалы, иностранные языки, профессиональное развитие или просто любопытство - наше расширение поможет вам углубить понимание и улучшить запоминание информации.</p>
+        </article>
+        <article>
+          <div className="title-1 relative font-bold" id="benefits">Преимущества</div>
+          <ul className="list text-white">
+            <li>
+                <span className="font-bold inline-flex"><BellIcon className="relative top-1 h-6 w-6 mr-1" aria-hidden="true" /> Организация знаний:</span> Создавайте топики и структурируйте концепции для более эффективного обучения.
+            </li>
+            <li>
+                <span className="font-bold inline-flex"><ClockIcon className="relative top-1 h-6 w-6 6 mr-1" aria-hidden="true" /> Гибкое расписание:</span> Установите интервалы появления концепций по таймеру, чтобы оптимизировать запоминание.
+            </li>
+            <li>
+                <span className="font-bold inline-flex"><LightBulbIcon className="relative top-1 h-6 w-6 mr-1" aria-hidden="true" /> Интеграция в браузер:</span> Попап-окна с концепциями появляются непосредственно на страницах, которые вы просматриваете.
+            </li>
+            <li>
+                <span className="font-bold inline-flex"><GlobeAltIcon className="relative top-1 h-6 w-6 mr-1" aria-hidden="true" /> Универсальность:</span> Применяйте расширение для изучения любой темы или предмета.
+            </li>
+            <li>
+                <span className="font-bold inline-flex"><BoltIcon className="relative top-1 h-6 w-6 mr-1" aria-hidden="true" /> Максимальная концентрация:</span> Избавьтесь от отвлекающих факторов и фокусируйтесь на существенных понятиях.
+            </li>
+          </ul>
+          <p className="max-width-858">Не ограничивайте свой потенциал - станьте мастером в обучении с помощью "Topic Meister"! Улучшите свои навыки запоминания и усвоения информации, повысьте эффективность своего учебного процесса и достигните новых высот в своих академических или профессиональных целях.</p>
+          <p className="font-bold mb-10">Установите "Topic Meister" сейчас и откройте<br /> новые горизонты обучения!</p>
+          <a href="#" className="install-btn text-lg font-bold py-4 px-9 inline-block bg-white rounded-xl hover:bg-slate-50 active:bg-slate-100">Установить</a>
+        </article>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <footer className="footer flex justify-center items-center max-w-screen-xl w-full border-t border-white pt-10 pb-14">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/logo-bot.png"
+          alt="logo-bot"
+          width={184}
+          height={55}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <span className="text-white font-bold">@{year}</span>
+      </footer>
     </main>
   )
 }
